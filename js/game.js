025275bb12e6d66c1f5fc2584fd9146d4d380ad3
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2024-02-26 01:03:28 +0330

    create function for geting the questions & answers

--- js/game.js
+++ js/game.js
@@ -1,14 +1,30 @@
+const loader=document.getElementById("loader");
+const container=document.getElementById("container");
+
 const URL= "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 let formattedData=null;
 
-const loader=document.getElementById("loader");
-const container=document.getElementById("container");
+const formatData= questionData =>{
+    const result = questionData.map((item)=>{
+        const questionObj={question : item.question};
+        const answers = [...item.incorrect_answers];
+        const correctAnswerIndex=Math.floor(Math.random() * 4);
+        answers.splice(correctAnswerIndex,0,item.correct_answer);
+        questionObj.answers=answers;
+        questionObj.correctAnswerIndex=correctAnswerIndex;
+        return questionObj;
+    })
+    return result;
+}
+
+
 
 const fetchData=async ()=>{
     const res= await fetch(URL);
     const json=await res.json();
     console.log(json)
-    formattedData=json;
+    formattedData=formatData(json.results)
+    console.log(formattedData)
     start()
 }
 
