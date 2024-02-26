f8f7ffdf4fb099c7e8a21b294017ae333afc960c
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2024-02-27 01:18:55 +0330

    handel the answer is correct or not

--- js/game.js
+++ js/game.js
@@ -1,59 +1,63 @@
-const loader=document.getElementById("loader");
-const container=document.getElementById("container");
-const questionText=document.getElementById("question-text");
-const answerList=document.querySelectorAll(".answer-text");
+const loader = document.getElementById("loader");
+const container = document.getElementById("container");
+const questionText = document.getElementById("question-text");
+const answerList = document.querySelectorAll(".answer-text");
 
+const URL =
+  "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 
-const URL= "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
+let formattedData = null;
+let questionIndex = 0;
+let correctAnswer = null;
 
-let formattedData=null;
-let questionIndex=0;
-let correctAnswer=null;
+const formatData = (questionData) => {
+  const result = questionData.map((item) => {
+    const questionObj = { question: item.question };
+    const answers = [...item.incorrect_answers];
+    const correctAnswerIndex = Math.floor(Math.random() * 4);
+    answers.splice(correctAnswerIndex, 0, item.correct_answer);
+    questionObj.answers = answers;
+    questionObj.correctAnswerIndex = correctAnswerIndex;
+    return questionObj;
+  });
+  return result;
+};
 
-const formatData= questionData =>{
-    const result = questionData.map((item)=>{
-        const questionObj={question : item.question};
-        const answers = [...item.incorrect_answers];
-        const correctAnswerIndex=Math.floor(Math.random() * 4);
-        answers.splice(correctAnswerIndex,0,item.correct_answer);
-        questionObj.answers=answers;
-        questionObj.correctAnswerIndex=correctAnswerIndex;
-        return questionObj;
-    })
-    return result;
-}
+const fetchData = async () => {
+  const res = await fetch(URL);
+  const json = await res.json();
+  console.log(json);
+  formattedData = formatData(json.results);
+  console.log(formattedData);
+  start();
+};
 
+const start = () => {
+  showQuestion();
+  loader.style.display = "none";
+  container.style.display = "block";
+};
 
-
-const fetchData=async ()=>{
-    const res= await fetch(URL);
-    const json=await res.json();
-    console.log(json)
-    formattedData=formatData(json.results)
-    console.log(formattedData)
-    start()
-}
-
-const start=()=>{
-    showQuestion();
-   loader.style.display="none";
-   container.style.display="block"
-}
-
-
-const showQuestion=()=>{
-    const{question,answers,correctAnswerIndex}=formattedData[questionIndex];
-    correctAnswer=correctAnswerIndex;
-    questionText.innerText=question;
-    answerList.forEach((button,index)=>{
-        button.innerText=answers[index];
-    })
-
-}
-const checkAnswer=()=>{
-    
-}
-window.addEventListener("load",fetchData)
-answerList.forEach((button,index)=>{
-    button.addEventListener("click",checkAnswer)
-})
+const showQuestion = () => {
+  const { question, answers, correctAnswerIndex } =
+    formattedData[questionIndex];
+  correctAnswer = correctAnswerIndex;
+  console.log(correctAnswer);
+  questionText.innerText = question;
+  answerList.forEach((button, index) => {
+    button.innerText = answers[index];
+  });
+};
+const checkAnswer = (event, index) => {
+  const isCorrect = index === correctAnswer ? true : false;
+  if (isCorrect) {
+    event.target.classList.add("correct");
+  } else {
+    event.target.classList.add("incorrect");
+    answerList[correctAnswer].classList.add("correct");
+  }
+};
+window.addEventListener("load", fetchData);
+answerList.forEach((button, index) => {
+  button.addEventListener("click", (event) => checkAnswer(event, index));
+});
